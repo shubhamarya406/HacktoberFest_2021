01aa990e166ba3b4984fbc504c8e7a50c3c8dd83
Manav Gosain <55010455+ManavGosain02@users.noreply.github.com>
2022-10-01 21:52:59 +0530

    Create Q4.java

---
+++ Java/Q4.java
@@ -0,0 +1,8 @@
+public class Q4 {
+    public static void main(String[] args) {
+        float n = (float) 2.3;
+        float m = (float) 4.2;
+
+        System.out.println("Product= " + (n * m));
+    }
+}
